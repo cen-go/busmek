1d85c85754c7542b7e18c3c8f8b3fa9f1ec9f701
cen-go <148121511+cen-go@users.noreply.github.com>
2024-05-15 12:49:11 +0300

    Add files via upload

---
+++ info.php
@@ -0,0 +1,59 @@
+<?php
+  // phpinfo();
+  // echo $name = "Cengiz";
+  // echo $age = 35;
+  // echo $isWorking = false;
+  // echo $pi = 3.14;
+  // $dizi = array("Ahmet", 25, false, 5.25);
+
+  // foreach($dizi as $d) {
+  //   echo $d;
+  // }
+
+  // $yas = 15;
+  // if ($yas >= 18) {
+  //   echo "yetişkin";
+  // } else {
+  //   echo "çocuk";
+  // }
+
+  $gun = "Pazar";
+  // if ($gun === "Cumartesi" || $gun === "Pazar") {
+  //   echo "haftasonu";
+  // } else {
+  //   echo "haftaiçi";
+  // }
+
+  // switch ($gun) {
+  //   case 'Pazartesi':
+  //   case 'Salı':
+  //   case 'Çarşamba':
+  //   case 'Perşembe':
+  //   case 'Cuma':
+  //     echo "Haftaiçi";
+  //     break;
+  //   case 'Cumartesi':
+  //   case 'Pazar':
+  //     echo "Haftasonu";
+  //     break;
+  //   default:
+  //     echo "Lütfen gün giriniz";
+  //     break;
+  // }
+  
+  // $a = 5;
+  // $b = 6;
+  // $c = 4;
+
+  // if ($a === $b && $b === $c) {
+  //   echo "eş kenar üçgen";
+  // } elseif ($a !== $b && $a !== $c && $b !== $c) {
+  //   echo "çeşit kenar üçgen";
+  // } else {
+  //   echo "ikiz kenar üçgen";
+  // }
+
+  $a = "5";
+  $yeniA = intval($a);  // ya da =>  (int)$a; floatval => float a dönüştürme, strval => stringe dönüstürme
+  
+?>
