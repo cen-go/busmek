26c855a4388b79d2ba85e29fef70aad7bd700d6d
cen-go <cengizulutas123123@gmail.com>
2024-05-20 16:07:56 +0300

    20 mayıs info

--- info.php
+++ info.php
@@ -84,12 +84,12 @@
   //   $i++;
   // }
 
-  $dizi = array(3, 5, 8, 15, 20);
-  $dizi.= [25];
+  // $dizi = array(3, 5, 8, 15, 20);
+  
 
-  foreach ($dizi as $i) {
-    echo $i."<br>";
-  }
+  // foreach ($dizi as $i) {
+  //   echo $i."<br>";
+  // }
   
 
   // $d = array(
@@ -101,6 +101,134 @@
   // foreach ($d as $key => $value) {
   //   echo "$key = $value"."<br>";
   // }
- 
 
+  // $d = ["ahmet", 54, "mehmet", 25];
+
+  // $d2 = ["ad" => "ahmet", "yas" => 54];
+
+  // var_dump($d);
+
+  // print_r($d);
+
+  // echo "<pre>";
+  //   print_r($d);
+  // echo "</pre>";
+
+  // var_dump($d2);
+
+  // print_r($d2);
+
+  // echo "<pre>";
+  //   print_r($d2);
+  // echo "</pre>";
+ 
+  // // echo $d[3]."<br>";
+  // // echo $d2["ad"];
+
+  // foreach ($d as $a) {
+  //   echo $a."<br>";
+  // }
+  
+  // foreach ($d2 as $k => $a) {
+  //   echo $k." = ".$a."<br>";
+  // }
+
+
+  // $d1 = ["ahmet", "mehmet", "fikret"];
+  // $d2 = [5, 7, 15, 25, 42, 5, 15, 7];
+  // $d3 = ["ad" => "ahmet", "yas" => 54];
+
+  // $sonuc1 = in_array("fikret", $d1);
+  // $sonuc2 = array_key_exists("yas", $d3);
+  // $sonuc3 = in_array("ahmet", $d3);
+  // $sonuc4 = array_key_exists("meslek", $d3);
+  
+  // var_dump($sonuc1);
+  // var_dump($sonuc2);
+  // var_dump($sonuc3);
+  // var_dump($sonuc4);
+
+  // $sorgu = is_array($d2);
+
+  // var_dump($sorgu);
+
+  // $birlestir = array_merge($d1, $d2);
+  // var_dump($birlestir);
+
+  // $unique = array_unique($birlestir);
+  // var_dump($unique);
+
+  /* ------------ ÇOKLU DİZİLER ------------- */
+
+//   $d = array(
+//     array(1, 2, 3),
+//     array(4, 5, 6),
+//     array(7, 8, 9)
+//   );
+
+//   for ($i=0; $i < 3; $i++) { 
+//     for ($j=0; $j < 3; $j++) { 
+//       echo "d dizisinin <strong>$i</strong> indexindeki, alt dizinin <strong>$j</strong> indexindeki elmanı = ".$d[$i][$j]."<br>";
+//     }
+//   }
+
+//   $a = array(
+//     array(
+//       array(1, 2),
+//       array(3, 4)
+//     ),
+//     array(
+//       array(5, 6),
+//       array(7, 8)
+//     )
+//   );
+
+// echo "<br>";
+
+//   for ($i=0; $i < 2; $i++) { 
+//     for ($j=0; $j < 2; $j++) { 
+//       for ($k=0; $k < 2; $k++) { 
+//         echo "a dizisi => <strong>$i
+//         </strong> indexindeki alt dizi => <strong>$j
+//         </strong> indexindeki alt dizinin <strong>$k
+//         </strong> 'nci elemanı = ".$a[$i][$j][$k]."<br>";
+//       }
+//     }
+//   }
+
+
+  $urunler = array(
+    "elektronik" => array(
+      "bilgisayar" => array(
+        array("marka" => "Acer", "fiyat" => 1000)
+      ),
+      "telefon" => array(
+        array("marka" => "iphone", "fiyat" => 1500)
+      )
+    ),
+    "giyim" => array(
+      "erkek" => array(
+        array("marka" => "Nike", "fiyat" => 500)
+      ),
+      "kadın" => array(
+        array("marka" => "Zara", "fiyat" => 800)
+      )
+    )
+  );
+
+
+  foreach ($urunler as $key1 => $kategoriler) {
+    echo $key1."<br>";
+    foreach ($kategoriler as $key2 => $altKategoriler) {
+      echo "<strong>".$key2."</strong>"."<br>";
+      foreach ($altKategoriler as $key3) {
+        echo "Marka = ".$key3["marka"]."<br>";
+        echo "Fiyat = ".$key3["fiyat"]."<br>";
+      }
+    }
+  }
+
+
+
+  
 ?>
